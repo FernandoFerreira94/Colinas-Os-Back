320f444ce61ffee1ea36a71a7a5e4cb56ceef219
Fernando <fernandoeqp59@gmail.com>
2026-04-01 20:08:47 -0300

    ok

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // URL do seu frontend
+    origin: [
+      'http://localhost:3000', // dev local (Next.js)
+      'https://colinas-os-front.vercel.app', // produção
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
-  });
+});
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
